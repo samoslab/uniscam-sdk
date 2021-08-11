6b44330eb5b855ff94db2f6f6fb5c2c00332010a
zhiyuan2021 <zhiyuan2021@gmail.com>
2021-08-11 08:37:33 +0800

    like mdx

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -24,8 +24,8 @@ export declare enum Rounding {
     ROUND_HALF_UP = 1,
     ROUND_UP = 2
 }
-export declare const FACTORY_ADDRESS = "0x0f24509be4af2f4c59f96dfa73812e73208792bb";
-export declare const INIT_CODE_HASH_BSC = "0x0fa78881436cb121aa18ae75c7bd50d9e517bb3f3cf4aa724fc948791f45b56e";
+export declare const FACTORY_ADDRESS = "0xf210b78636ba74f21eb859b2d0601087814bc151";
+export declare const INIT_CODE_HASH_BSC = "0x3fcf43bd51e48d195931618953d09689ad8616542514793d4e8991fb7603da41";
 export declare const INIT_CODE_HASH_ETH = "0xf72b1bfa0de188ad1a4b71bb308a2c57e71c358a10cbbac6e7a820f3e4ba004d";
 export declare const INIT_CODE_HASH_HECO = "0x0fa78881436cb121aa18ae75c7bd50d9e517bb3f3cf4aa724fc948791f45b56e";
 export declare const INIT_CODE_HASH_MATIC = "0xf72b1bfa0de188ad1a4b71bb308a2c57e71c358a10cbbac6e7a820f3e4ba004d";
